5d3b049a175f93b5f92d69105599c1e9d8dd5b19
Iuliia Goriun <goriunyuliya@gmail.com>
2023-02-24 15:14:42 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,24 +9,23 @@ import StyledContainer from './General.styled';
 export function App() {
   const [contacts, setContacts] = useState(
     getLocalData() ?? [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]
+  );
   const [filter, setFilter] = useState('');
-  
 
-  function getLocalData () {
+  function getLocalData() {
     const savedContacts = JSON.parse(localStorage.getItem('contacts'));
-   return savedContacts;
+    return savedContacts;
   }
   useEffect(() => {
-    
-      // if (prevState.contacts !== contacts) {
-        localStorage.setItem('contacts', JSON.stringify(contacts))
-      }, [contacts])
-  
+    // if (prevState.contacts !== contacts) {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
   const formSubmitHandler = contact => {
     const isExist = contacts.some(({ name }) => {
       return contact.name === name;
@@ -35,44 +34,32 @@ export function App() {
       alert(`${contact.name} is already in contacts`);
       return;
     }
-    setContacts([...contacts, { ...contact, id: nanoid() }],
-      )};
-    
+    setContacts([...contacts, { ...contact, id: nanoid() }]);
+  };
+
   const deleteItem = event => {
-    setContacts(contacts => 
-        contacts.filter(
-          contact => contact.id !== event.target.id
-        ),);
+    setContacts(contacts =>
+      contacts.filter(contact => contact.id !== event.target.id)
+    );
   };
   const handleFilterChange = event => {
     const { value } = event.target;
     setFilter(value);
   };
 
-  
-    const filteredContacts = contacts.filter(contact =>
-      contact.name
-        .toLowerCase()
-        .trim()
-        .includes(filter.toLowerCase())
-    );
-    return (
-      <>
-        <StyledContainer>
-          <h1>Phonebook</h1>
-          <ContactForm onSubmit={formSubmitHandler} />
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().trim().includes(filter.toLowerCase())
+  );
+  return (
+    <>
+      <StyledContainer>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={formSubmitHandler} />
 
-          <h2>Contacts</h2>
-          <Filter
-            onChange={handleFilterChange}
-            value={filter}
-          />
-          <ContactList
-            contacts={filteredContacts}
-            handleDelete={deleteItem}
-          />
-        </StyledContainer>
-      </>
-    );
-  }
-
+        <h2>Contacts</h2>
+        <Filter onChange={handleFilterChange} value={filter} />
+        <ContactList contacts={filteredContacts} handleDelete={deleteItem} />
+      </StyledContainer>
+    </>
+  );
+}
